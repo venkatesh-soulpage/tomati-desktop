990b642adee100fa6da8db31ffef2b95c148a359
Venkatesh <venkatesh.marreboyina@soulpageit.com>
2021-02-25 00:44:09 +0530

    Added staging config

--- src/utils/axios_configuration.js
+++ src/utils/axios_configuration.js
@@ -1,14 +1,19 @@
 import axios from "axios";
-import { AWS_API_URL, HEROKU_API_URL } from "constants/APIRoutes";
+import {
+  AWS_API_URL,
+  HEROKU_API_URL,
+  AWS_STAGING_API_URL,
+} from "constants/APIRoutes";
 
 if (process.env.REACT_APP_VERCEL) {
   axios.defaults.baseURL = HEROKU_API_URL;
 } else if (process.env.REACT_APP_AWS) {
   axios.defaults.baseURL = AWS_API_URL;
+} else if (process.env.REACT_APP_AWS_STAGING) {
+  axios.defaults.baseURL = AWS_STAGING_API_URL;
 } else {
   axios.defaults.baseURL = "http://127.0.0.1:3000";
 }
-
 (function () {
   var token = sessionStorage.getItem("token");
   if (token) {
