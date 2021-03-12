e20389f7b048e3e3dc58ad19479fb346c007f406
unknown <david.prafull@soulpageit.com>
2021-03-12 17:01:00 +0530

    No collaborators message

--- src/pages/Collaborators/index.js
+++ src/pages/Collaborators/index.js
@@ -10,31 +10,39 @@ const Index = (props) => {
 
   return (
     <>
-      {props?.outlet?.outlet?.collaborators?.map((collaborator, id) => {
-        return (
-          <div key={id} className="p-2 px-0 m-0">
-            <div>
-              <div
-                className="card px-4 py-4 mt-1 cr-p"
-                style={{ borderRadius: 6 }}
-              >
-                <div className="d-flex align-items-center justify-content-between">
-                  <div>
-                    <h6 className="m-0 font-weight-bold">
-                      {collaborator.email}
-                    </h6>
-                  </div>
-                  <div>
-                    <button className="btn btn-danger">
-                      {collaborator.status}
-                    </button>
+      {props?.outlet?.outlet?.collaborators?.length === 0 ? (
+        <div className="text-center mt-5">
+          you currently have no collaborators, add some
+        </div>
+      ) : (
+        <div>
+          {props?.outlet?.outlet?.collaborators?.map((collaborator, id) => {
+            return (
+              <div key={id} className="p-2 px-0 m-0">
+                <div>
+                  <div
+                    className="card px-4 py-4 mt-1 cr-p"
+                    style={{ borderRadius: 6 }}
+                  >
+                    <div className="d-flex align-items-center justify-content-between">
+                      <div>
+                        <h6 className="m-0 font-weight-bold">
+                          {collaborator.email}
+                        </h6>
+                      </div>
+                      <div>
+                        <button className="btn btn-danger">
+                          {collaborator.status}
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        );
-      })}
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
