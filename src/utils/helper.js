64f9f9ce347547a03e3cf04806e594113cfbaaf9
unknown <david.prafull@soulpageit.com>
2021-03-17 19:54:14 +0530

    Download QR and Hotjar update

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -29,7 +29,38 @@ export function downloadURI(uri, name) {
   document.body.removeChild(link);
 }
 
-export function downloadQr(name) {
+const downloadQr2 = (qrcode, name, doc, pdfWidth) => {
+  const downloadedImg = new Image();
+  downloadedImg.crossOrigin = "Anonymous";
+  downloadedImg.addEventListener(
+    "load",
+    function () {
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      canvas.width = downloadedImg.width;
+      canvas.height = downloadedImg.height;
+      context.drawImage(downloadedImg, 0, 0);
+      doc.addImage(
+        canvas.toDataURL("image/png"),
+        "PNG",
+        pdfWidth / 2 - 50,
+        0,
+        100,
+        100
+      );
+      doc.setFont("helvetica", "bold");
+      doc.setFontSize(20);
+      doc.text(name, pdfWidth / 2 - doc.getTextWidth(name) / 2, 101, {
+        align: "justify",
+      });
+      doc.save(`${name}.pdf`);
+    },
+    false
+  );
+  downloadedImg.src = qrcode.src;
+};
+
+export async function downloadQr(name) {
   const qrcode = document.querySelector("#menu-qr");
   const options = {
     orientation: "p",
@@ -37,8 +68,11 @@ export function downloadQr(name) {
   };
   const doc = new jsPDF(options);
   const pdfWidth = doc.internal.pageSize.getWidth();
-
-  doc.addImage(qrcode.src, "PNG", pdfWidth / 2 - 50, 0, 100, 100);
+  try {
+    doc.addImage(qrcode.src, "PNG", pdfWidth / 2 - 50, 0, 100, 100);
+  } catch (err) {
+    downloadQr2(qrcode, name, doc, pdfWidth);
+  }
 
   doc.setFont("helvetica", "bold");
   doc.setFontSize(20);
