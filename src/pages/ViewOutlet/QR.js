bc24dc83788afabcdd4dad664d49839472e8d527
saikrishna-soulpage <saikrishna.bandhu@soulpageit.com>
2021-04-06 17:23:37 +0530

    csv template update

--- src/pages/ViewOutlet/QR.js
+++ src/pages/ViewOutlet/QR.js
@@ -6,7 +6,7 @@ const QR = ({ outlet }) => {
   return (
     <div className="card bg-white border p-5 mt-2">
       <div className="d-flex align-items-top">
-        <div className="mr-5">
+        <div className="mr-5 image-div">
           <img
             alt="menu-qr"
             className="border"
@@ -15,14 +15,8 @@ const QR = ({ outlet }) => {
             width="204px"
             id="menu-qr"
           />
-        </div>
-        <div className="mr-auto">
-          <h4 className="text-dark">{outlet?.name}</h4>
-          <p className="text-dark font-weight-light w-56">
-            {outlet?.description}
-          </p>
-          <button
-            className="btn btn-danger mr-3 mt-1 rounded-pill"
+          <div
+            className="cr-p a-link overlay"
             onClick={() =>
               getImgFromUrl(outlet, function (img, name, formattedName) {
                 generatePDF(img, name, formattedName);
@@ -30,13 +24,22 @@ const QR = ({ outlet }) => {
             }
           >
             Download QR Code
-          </button>
+          </div>
+        </div>
+        <div className="mr-auto">
+          <h4 className="text-dark">{outlet?.name}</h4>
+          <p className="text-dark font-weight-light w-56">
+            {outlet?.description}
+          </p>
           <a
-            className="a-link btn btn-outline-dark mt-1 px-4 rounded-pill"
+            className=" btn btn-danger mr-3 mt-1 rounded-pill"
             href={CSV_TEMPLATE_URL}
           >
             Menu Template
           </a>
+          <button className="btn btn-outline-dark mt-1 px-4 rounded-pill ">
+            Order Menu tags
+          </button>
         </div>
       </div>
     </div>
